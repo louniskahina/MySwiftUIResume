ad4a2b947da9653fa148826b3c938d88c089c0eb
kahina lounis <kahinalounis2014@outlook.com>
2024-10-17 15:19:22 +0200

    Initial Commit

---
+++ KahinaResume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KahinaResume
+//
+//  Created by Kahina Lounis on 17/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KahinaResume/KahinaResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KahinaResumeApp.swift
+//  KahinaResume
+//
+//  Created by Kahina Lounis on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct KahinaResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
