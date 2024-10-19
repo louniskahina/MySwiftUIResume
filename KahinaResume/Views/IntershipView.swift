a3b23f33f45185ce0bc95025539f88ca7147c211
kahina lounis <kahinalounis2014@outlook.com>
2024-10-19 15:35:05 +0200

    custom displayed font in home page

---
+++ KahinaResume/Views/IntershipView.swift
@@ -0,0 +1,8 @@
+//
+//  IntershipView.swift
+//  KahinaResume
+//
+//  Created by Kahina Lounis on 19/10/2024.
+//
+
+import Foundation
---
+++ KahinaResume/Views/SkillsView.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  KahinaResume
+//
+//  Created by Kahina Lounis on 18/10/2024.
+//
+
